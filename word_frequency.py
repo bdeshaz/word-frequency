3b6617f7babe3d3789bb16934c0898587b2d8c7d
Brian <bdeshaz@gmail.com>
2015-05-06 20:41:54 -0400

    added filter

--- word_frequency.py
+++ word_frequency.py
@@ -1,30 +1,48 @@
 import re
 
-
-""" clean_text(text)/ get_words(text)
-
-
-word_frequency(text)
-
-if __name__=="__main__"
-    read file
-    call word_frequency
-    call print_word_freq """
-
-
 """ Text of doc """
 with open("sample.txt") as sample:
     lines = sample.readlines()
+
+""" Removes Words from List """
+
+def word_removal(list_o_words):
+
+    useless_words = """a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,
+    because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,
+    for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,
+    it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,
+    not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,
+    since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,
+    twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,
+    would,yet,you,your"""
+
+    clean_useless_words = re.sub(r'[^A-Za-z]', "\n", useless_words)
+    useless_list = clean_useless_words.split()
+
+    clean_list = []
+
+    useless_set = set(useless_list)
+    for word in list_o_words:
+        if word not in useless_set:
+            clean_list.append(word)
+            
+    return clean_list
+    # print(clean_list)
+    # return filter(lambda list_o_words: useless_list, list_o_words)
+
+
+# word_removal(['a', 'dog','is','the','animal'])
+
 
 
 def word_frequency(text):
 
     """ Creates List from inputed text """
-    # text = text.lower()
-    # text_on_one_line = re.split(' ',text)
-    # clean_text = re.sub(r'[^A-Za-z]', "\n", (' '.join(text_on_one_line)).lower()
     clean_text = re.sub(r'[^A-Za-z]', "\n", (' '.join(text)).lower())
     text_list = clean_text.split()
+
+    text_list = word_removal(text_list)
 
     """ Create Dictionary for count """
     text_dict = {}
@@ -40,9 +58,4 @@ def word_frequency(text):
         print(word, count)
 
 
-
-
-
-
-# print(a_few_words)
 word_frequency(lines)
